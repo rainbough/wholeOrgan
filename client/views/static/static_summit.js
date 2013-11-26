4656e3637b7a6872fe0e0498cd2501bca1aff97a
Amanda Landes Macbook3 <asaffer@landesbioscience.com>
2013-11-26 17:26:36 -0600

    added bubble images to images dir and trying to use gmaps.js

--- client/views/static/static_summit.js
+++ client/views/static/static_summit.js
@@ -2,7 +2,13 @@
 // follow this example (replacing static_summit with the corresponding template name) for other pages
 // if you for whatever reason want to make a file ouytside of the static folder, please tell me first
 Template.static_summit.rendered = function() {
-	// code goes here (haha)
 
 	$(".help-block").popover();
+
+	new GMaps({
+  		div: '#summit_map',
+  		lat: -12.043333,
+  		lng: -77.028333
+	});
+	
 }
