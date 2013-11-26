10037ddbcfcce00868cda823722126089c836a23
jackson <capcolors@nocchi.(none)>
2013-11-26 16:06:17 -0600

    added js so amanda can tweak things clientside

---
+++ client/views/static/static_summit.js
@@ -0,0 +1,6 @@
+// this is basically the meteor version of $(document).ready()
+// follow this example (replacing static_summit with the corresponding template name) for other pages
+// if you for whatever reason want to make a file ouytside of the static folder, please tell me first
+Template.static_summit.rendered = function() {
+	// code goes here (haha)
+}
